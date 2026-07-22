1f0adebc580054458e8ecc1bed0ad0b01c383d28
thinh-tr <ducthinh2049@gmail.com>
2026-07-22 09:26:02 +0700

    feat: bổ sung toàn bộ các schema của các entities còn lại trong maria-entities.ts

--- src/maria-schema.ts
+++ src/maria-schema.ts
@@ -4,7 +4,7 @@
 
 import { InsertableSchema } from "./maria-services.ts";
 import { Log } from "./sys.ts";
-import { Page, PageStatus } from "./maria-entities.ts";
+import { Page, PageStatus, PageMeta, HtmlContent, HtmlHash } from "./maria-entities.ts";
 
 // Schema cho hệ thống Log
 export const logSchema: InsertableSchema<Log> = {
@@ -19,4 +19,19 @@ export const pageSchema: InsertableSchema<Page> = {
 // Schema cho kiểu PageStatus
 export const pageStatusSchema: InsertableSchema<PageStatus> = {
     fields: ["id", "pageId", "createdTimestamp", "updateTimestamp"],
+};
+
+// Schema cho kiểu PageMeta
+export const pageMetaSchema: InsertableSchema<PageMeta> = {
+    fields: ["id", "pageId", "title", "publicationTimestamp", "pageType", "source", "language"],
+};
+
+// Schema cho kiểu HtmlContent
+export const htmlContentSchema: InsertableSchema<HtmlContent> = {
+    fields: ["id", "pageId", "htmlData"],
+};
+
+// Schema cho kiểu HtmlHash
+export const htmlHashSchema: InsertableSchema<HtmlHash> = {
+    fields: ["id", "pageId", "hash"],
 };
